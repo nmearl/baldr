4d9bf621b4dd6ee18140793a39bd849008389a8a
nmearl <contact@nicholasearl.me>
2019-08-14 09:42:30 -0400

    Switch nav drawer to vue template style

---
+++ baldr/widgets/navigation_drawer.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-navigation-drawer
+            v-model="drawer"
+            :mini-variant.sync="mini"
+            app
+            fixed
+            clipped
+    >
+        <v-toolbar flat class="transparent">
+            <v-list class="pa-0">
+
+                <v-list-tile-action>
+                    <v-btn
+                            icon
+                            @click.stop="mini = !mini"
+                    >
+                        <v-icon>chevron_left</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list>
+
+
+            <!-- <v-list class="pa-0">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                        <img src="https://randomuser.me/api/portraits/men/85.jpg">
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>John Leider</v-list-tile-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                        <v-btn
+                                icon
+                                @click.stop="mini = !mini"
+                        >
+                            <v-icon>chevron_left</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list> -->
+        </v-toolbar>
+
+        <v-list class="pt-0" dense>
+            <v-list-group prepend-icon='dashboard' no-action>
+                <template v-slot:activator>
+                    <v-list-tile>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Data</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+
+                <v-list-tile
+                        v-for="item in data_items"
+                        :key="item.title"
+                        @click=""
+                >
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list-group>
+        </v-list>
+    </v-navigation-drawer>
+</template>
