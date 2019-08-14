adf333c2b315c52883c5c539e05a32653d8a4c7b
nmearl <contact@nicholasearl.me>
2019-08-14 09:41:21 -0400

    Improve navigation drawer population behavior

---
+++ baldr/widgets/toolbar.vue
@@ -0,0 +1,23 @@
+<v-toolbar dark app color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+        <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+        <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+        <v-icon>refresh</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+        <v-icon>more_vert</v-icon>
+    </v-btn>
+</v-toolbar>
