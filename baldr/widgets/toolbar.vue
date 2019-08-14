8ec68219935206f832cb647ddd298f2ebee719da
nmearl <contact@nicholasearl.me>
2019-08-14 09:43:00 -0400

    Move toolbar to vue style template

--- baldr/widgets/toolbar.vue
+++ baldr/widgets/toolbar.vue
@@ -1,23 +1,41 @@
-<v-toolbar dark app color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
+<template>
+    <v-toolbar fixed dark app clipped-left color="primary">
+        <v-btn outline>
+            Open
+        </v-btn>
+        <v-btn outline>
+            New
+        </v-btn>
+        <v-btn outline>
+            Save
+        </v-btn>
 
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-divider vertical></v-divider>
 
-    <v-spacer></v-spacer>
+        <v-overflow-btn
+            dark=false
+            hide-details
+            label="Overflow Btn"
+        ></v-overflow-btn>
 
-    <v-btn icon>
-        <v-icon>search</v-icon>
-    </v-btn>
+        <v-divider vertical></v-divider>
+        <v-spacer></v-spacer>
 
-    <v-btn icon>
-        <v-icon>apps</v-icon>
-    </v-btn>
+        <v-btn icon>
+            <v-icon>search</v-icon>
+        </v-btn>
 
-    <v-btn icon>
-        <v-icon>refresh</v-icon>
-    </v-btn>
+        <v-btn icon>
+            <v-icon>apps</v-icon>
+        </v-btn>
 
-    <v-btn icon>
-        <v-icon>more_vert</v-icon>
-    </v-btn>
-</v-toolbar>
+        <v-btn icon>
+            <v-icon>refresh</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+            <v-icon>more_vert</v-icon>
+        </v-btn>
+    </v-toolbar>
+</template>
